e2bd5d3cc334236c9a7c52cfa52fcd0b284b29c9
Bastian Jensen <bastianjensen@icloud.com>
2019-12-01 02:51:38 -0300

    Crea una copia del archivo con fecha en formato Date, al que se le agrega una primera fila con keys (data,battery,signal,variation) para ser usada con Dash, ademas se sige modificando el documento python de Dash

--- procesamiento_visalizacion/dash_radio_telescope.py
+++ procesamiento_visalizacion/dash_radio_telescope.py
@@ -6,15 +6,112 @@ Created on Sat Nov 30 23:21:25 2019
 @author: bastianjensen
 """
 
+
+"""
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
+## https://github.com/bastianjensen/PT_UNAB/blob/procesamiento/radio_data_sun_CONVERTED_TO_DATE.csv
+
+
+
+
+## GRAFICA
+
 
 app = dash.Dash()
+
+
 app.layout = html.Div(children=[
-  html.H1(children='ráficos generados a partir de radio telescopio'),
-  dcc.Graph()])
+  html.H1(children='Gráficos generados a partir de radio telescopio'),
+  
+  dcc.Graph(id='ejemplo',
+    figure={
+      'data': [
+        {'x': [1, 2, 3, 4], 'y': [1, 8, 3, 7], 'type': 'line', 'name': 'Bicicletas'},
+        {'x': [1, 2, 3, 4], 'y': [5, 2, 8, 8], 'type': 'bar', 'name': 'Bicicletas electricas'},
+        ],
+      'layout': {
+      'title': 'Ejemplo básico en Dash'
+        }
+      })
+
+  ])
+
+
     
 if __name__ == '__main__':
   app.run_server(debug=True)
+  
+  
+"""
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+import pandas as pd
+#https://raw.githubusercontent.com/bastianjensen/PT_UNAB/master/radio_data_sun_MEDIA_10000.csv
+df = pd.read_csv('https://raw.githubusercontent.com/bastianjensen/PT_UNAB/master/radio_data_sun_MEDIA_10000.csv')  
+#df = pd.read_csv('https://raw.githubusercontent.com/bastianjensen/PT_UNAB/procesamiento/radio_data_sun_CONVERTED_TO_DATE.csv')  
+
+#df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    dcc.Graph(id='graph-with-slider'),
+    dcc.Slider(
+        id='year-slider',
+        min=df['year'].min(),
+        max=df['year'].max(),
+        value=df['year'].min(),
+        marks={str(year): str(year) for year in df['year'].unique()},
+        step=None
+    )
+])
+
+
+@app.callback(
+    Output('graph-with-slider', 'figure'),
+    [Input('year-slider', 'value')])
+def update_figure(selected_year):
+    filtered_df = df[df.year == selected_year]
+    traces = []
+    for i in filtered_df.continent.unique():
+        df_by_continent = filtered_df[filtered_df['continent'] == i]
+        traces.append(dict(
+            x=df_by_continent['gdpPercap'],
+            y=df_by_continent['lifeExp'],
+            text=df_by_continent['country'],
+            mode='markers',
+            opacity=0.7,
+            marker={
+                'size': 15,
+                'line': {'width': 0.5, 'color': 'white'}
+            },
+            name=i
+        ))
+
+    return {
+        'data': traces,
+        'layout': dict(
+            xaxis={'type': 'log', 'title': 'GDP Per Capita',
+                   'range':[2.3, 4.8]},
+            yaxis={'title': 'Life Expectancy', 'range': [20, 90]},
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+            legend={'x': 0, 'y': 1},
+            hovermode='closest',
+            transition = {'duration': 500},
+        )
+    }
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+  
